5853d003a3de24f93c0594432dab4af78c3ac71c
luis andrade <andradex.js07@gmail.com>
2021-05-30 06:37:09 -0400

    firt commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e0464abec1a2c569cc0af9de5f28b217'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aeffd54d-d0dd-41ea-b9fc-7a3ae8fcbcab'
+};
